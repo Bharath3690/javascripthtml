4a4f0baf6766d22598c662614ed6456261f786a2
Bharath3690 <115339747+Bharath3690@users.noreply.github.com>
2022-12-06 19:26:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+console.log(3**2) //single comment line
+//here when we run the programm its show in chrome console
+console.log(3
+    
+    +
+    1)  //multiline comments
+
+alert('lets study javascript')
+//alert states the alert the message in the web page
+
+
+//variable
+let message
+message='i love my country'
+alert(message)
+
+
+let num=12
+    age=15
+    name =bharath
+console.log(num,age,name)
+
+
+//Data Types
+//8 types of datatypes
+
+//Number
+let n = 123
+n = 20.1234
+//a number can represent both integer and floating point numbers
+
+//special numeric values 
+
+// alert(1 / 0) //Infinity
+
+// alert('text' / 3)  NaN not a number
+
+// var a = '20'
+// var b = 2
+
+
+//BigInt
+//(2power53 - 1) , -(2power53 - 1)
+
+// const bigInt = 31827638126382716381263812638161326813618368126338126381263812638126312836821n
+
+// console.log(typeof bigInt)
+
+//string
+
+//undefined
+//value is not assigned
+let age;
+
+alert(age)
+
+
+
+
+
+
+
