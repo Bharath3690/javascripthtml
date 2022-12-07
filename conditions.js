7b22a4026eafe06d9009b26e6fc20bc3948bf53b
Bharath3690 <115339747+Bharath3690@users.noreply.github.com>
2022-12-07 23:49:35 +0530

    Add files via upload

---
+++ conditions.js
@@ -0,0 +1,20 @@
+let age=23
+
+if (age >= 18) {
+    console.log("you are eligible to vote")
+}else if (age<18){
+    console.log("you are not eligible")
+}
+
+let year=2021
+
+if (year==2022) {
+    console.log("leap year")
+} else {
+    console.log("not a leap year")
+}
+console.log(25/5)
+
+
+
+
---
+++ operators.js
@@ -0,0 +1,14 @@
+//console.log('1'+2)
+//in above addition case the string and number cant be add in 
+//this case
+//console.log(6-'4')
+
+console.log('345'+12322)
+console.log(55+76)
+console.log('5657'-674)
+console.log('56'*767)
+console.log('876'/670)
+console.log('3'**3)
+console.log(765-89)
+//in these operators we can use like this
+//in above case the subtraction str and num sub,div,mul
